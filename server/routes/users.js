2e7e7a94bff7465ad36a451b3e926e869debfe31
angelikakuleta <kuleta.an@gmail.com>
2019-04-19 19:59:49 +0200

    add CRUD operations on users

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const sql = require('mssql');
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
-const pool = require('../connection');
 
 const router = express.Router();
 
@@ -16,34 +15,90 @@ function validateUser(user) {
     return Joi.validate(user, schema);
 }
 
-router.post('/add', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
+    try {
+        let request = new sql.Request();
+
+        let user = await request
+            .query(`select * from Users where UserId = ${req.params.id}`)
+        if (!user.recordset[0]) return res.status(404).send('User does not exist.')
+
+        return res.status(200).send(user.recordset[0]);
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.post('/', async (req, res, next) => {
     const { error } = validateUser(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
 
     try {
-        const conn = await pool.connect();
+        let request = new sql.Request();
 
-        let user = await conn.request()
+        let user = await request
             .query(`select * from Users where UserEmail = '${req.body.email}'`)
-        if (user.recordset.length > 0) {
-            pool.close();
-            res.status(400).send('User already registered.');
-        }
-        
+        if (user.recordset[0]) return res.status(400).send('User already registered.');
+         
         const salt = await bcrypt.genSalt(10);
         const passwd = await bcrypt.hash(req.body.passwd, salt);
 
-        await conn.request()
+        await request
             .input('name', sql.NVarChar(50), req.body.name)
             .input('email', sql.NVarChar(50), req.body.email)
             .input('passwd', sql.NVarChar(1024), passwd)
             .query('insert into Users (UserName, UserEmail, UserPasswd, UserEXP, UserLevel) values (@name, @email, @passwd, 0, 1)');
-        
-        pool.close();
-        res.status(200).send('User was created.');       
+        return res.status(200).send('User was created.');
     } catch (err) {
-        pool.close();
-        res.status(400).send(err.originalError);
+        next(err)
+    }
+});
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        let request = new sql.Request();
+
+        let user = await request
+            .query(`select * from Users where UserId = '${req.params.id}'`)
+        if (!user.recordset[0]) return res.status(404).send('User does not exist.')
+
+        let name, email, passwd; 
+        name = (req.body.name) ? req.body.name : user.recordset[0].UserName;
+        email = (req.body.email) ? req.body.email : user.recordset[0].UserEmail;
+        passwd = (req.body.passwd) ? req.body.passwd : 'test123';
+
+        const { error } = validateUser({ name, email, passwd });
+        if (error) return res.status(400).send(error.details[0].message);
+        
+        if (req.body.passwd) {
+            const salt = await bcrypt.genSalt(10);
+            passwd = await bcrypt.hash(req.body.passwd, salt);
+        }
+        else { passwd = user.recordset[0].UserPasswd}
+
+        await request
+            .input('name', sql.NVarChar(50), name)
+            .input('email', sql.NVarChar(50), email)
+            .query(`update Users set UserName = @name, UserEmail = @email, UserPasswd = '${passwd}' WHERE UserId = ${req.params.id}`);
+        return res.status(200).send('User was updated.');
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        let request = new sql.Request();
+
+        let user = await request
+            .query(`select * from Users where UserEmail = '${req.body.email}'`)
+        if (user.recordset[0]) return res.status(404).send('User does not exist.');
+
+        await request
+            .query(`delete from Users where UserId = ${req.params.id}`)
+        return res.status(200).send('User was deleted.');
+    } catch (err) {
+        next(err)
     }
 });
 

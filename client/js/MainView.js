84014f9b0bd92474f80e77145764c1e685eeb867
Gustaw <tanski.gustaw@gmail.com>
2019-04-23 18:23:53 +0200

    [Insert your commit message here. Be sure to make it descriptive.]

--- client/js/MainView.js
+++ client/js/MainView.js
@@ -8,27 +8,44 @@ class MainView {
         } else {
             // tutaj tworzenie kategorii na podstawie danych z serwera
         }
+
         this._container = document.createElement("div");
-        this._container.classList.add("main-view");
+        this._container.innerHTML = `
+            <div id="main-view-bar" class="main-view-bar"></div>
+            <div id="main-view-wrapper" class="main-view-wrapper"></div>
+        `;
+
+        this._listWrapper = this._container.querySelector("#main-view-wrapper");
+        this._topBar = this._container.querySelector("#main-view-bar");
+
+        this._topBar.style.backgroundColor = "#55f";
+        this._topBar.innerText = "Jestem barem;"
 
         this._categoryButton = this._addButtonNewCategory();
-        this._container.appendChild(this._categoryButton);
+        this._listWrapper.appendChild(this._categoryButton);
         this._categoryButton.addEventListener('click', this._showInputName.bind(this));
+
+        this._isArchivedShown = false;
+
 
         // buttony do testowania kodu
 
-        this.helpfulButtonI = this._addButtonNewCategory();
-        this._container.appendChild(this.helpfulButtonI);
-        this.helpfulButtonI.innerText = "💩";
-        this.helpfulButtonI.addEventListener('click', () => {
-            this._showList(this._archivedCategoriesList);
+        this.activedButton = this._addButtonNewCategory();
+        this._listWrapper.appendChild(this.activedButton);
+        this.activedButton.innerText = "Aktywne";
+        this.activedButton.classList.add("login-button");
+        this.activedButton.addEventListener('click', () =>
+        {
+            this._showActiveCategories();
         });
-
-        this.helpfulButtonII = this._addButtonNewCategory();
-        this._container.appendChild(this.helpfulButtonII);
-        this.helpfulButtonII.innerText = "🔥";
-        this.helpfulButtonII.addEventListener('click', () => {
-            this._showList(this._categoriesList);
+        
+        this.archivedButton = this._addButtonNewCategory();
+        this._listWrapper.appendChild(this.archivedButton);
+        this.archivedButton.classList.add("login-button");
+        this.archivedButton.innerText = "Zarchiwizowane";
+        this.archivedButton.addEventListener('click', () =>
+        {
+            this._showArchivedCategories();
         });
 
         // this.helpfulButtonIII = this._addButtonNewCategory();
@@ -63,34 +80,38 @@ class MainView {
     }
 
     static createInputName() {
-        const formName = document.createElement('form')
-        formName.classList.add('form-category-name')
-        const inputName = document.createElement('input')
-        inputName.placeholder = "Wpisz nazwę kategorii."
-        inputName.classList.add('input-category-name')
-        inputName.id = 'add-category-input'
-        const inputButton = document.createElement('button')
-        inputButton.classList.add('button-category-name')
-        inputButton.innerText = 'Dodaj'
-        const deleteFormButton = document.createElement('div')
-        deleteFormButton.innerHTML = '<i class="fas fa-times"></i>'
-        formName.appendChild(inputName)
-        formName.appendChild(inputButton)
-        formName.appendChild(deleteFormButton)
-        deleteFormButton.addEventListener('click', function() {
-            if (this.parentElement.parentElement === document.querySelector('.category-header')) {
+        const formName = document.createElement('form');
+        formName.classList.add('form-category-name');
+        const inputName = document.createElement('input');
+        inputName.placeholder = "Wpisz nazwę kategorii.";
+        inputName.classList.add('input-category-name');
+        inputName.id = 'add-category-input';
+        const inputButton = document.createElement('button');
+        inputButton.classList.add('button-category-name');
+        inputButton.innerText = 'Dodaj';
+        const deleteFormButton = document.createElement('div');
+        deleteFormButton.innerHTML = '<i class="fas fa-times"></i>';
+        formName.appendChild(inputName);
+        formName.appendChild(inputButton);
+        formName.appendChild(deleteFormButton);
+        deleteFormButton.addEventListener('click', function() 
+        {
+            if (this.parentElement.parentElement === document.querySelector('.category-header')) 
+            {
                 this.parentElement.parentElement.firstElementChild.hidden = false;
             }
-            this.parentElement.remove()
+            this.parentElement.remove();
         })
-        return formName
+        return formName;
     }
 
     _showInputName() {
-        if (document.getElementById('add-category-input')) return
+        if (document.getElementById('add-category-input')) return;
         const formName = MainView.createInputName();
-        this._container.appendChild(formName);
+        this._listWrapper.appendChild(formName);
         formName.children[1].addEventListener('click', this._createNewCategory.bind(this));
+        formName.children[0].focus();
+
     }
 
     _createNewCategory(e) {
@@ -104,17 +125,36 @@ class MainView {
             _creationDate: new Date().getTime()
         })
         this._categoriesList.push(category);
-        this._container.appendChild(category.render());
+        this._listWrapper.appendChild(category.render());
         input.parentElement.remove();
     }
 
-    _showList(list) {
-
-        [...this._container.getElementsByClassName("category")].forEach(category => category.remove());
-        list.forEach(category => this._container.appendChild(category.render()));
+    _showArchivedCategories()
+    {
+        if (this._isArchivedShown) return;
+        this._isArchivedShown = true;
+        this._categoryButton.hidden = true;
+        this._showList(this._archivedCategoriesList);
+    }
+    
+    _showActiveCategories()
+    {
+        if (!this._isArchivedShown) return;
+        this._isArchivedShown = false;
+        this._categoryButton.hidden = false;
+        this._showList(this._categoriesList);
     }
 
-    archiveCategory(category) {
+    _showList(list)
+    {
+
+        [...this._listWrapper.getElementsByClassName("category")].forEach(category => category.remove());
+        list.forEach(category => this._listWrapper.appendChild(category.render()));
+    }
+
+
+    archiveCategory(category)
+    {
         this._archivedCategoriesList.push(...this._categoriesList.splice(category.index, 1));
         category.archive();
         category.index = this._archivedCategoriesList.length - 1;

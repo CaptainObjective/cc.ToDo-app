3ddec1235ee19aa69a1ceab6ee26295359e9d15e
Gustaw <tanski.gustaw@gmail.com>
2019-04-28 22:05:35 +0200

    Now it's working :D

--- client/js/main.js
+++ client/js/main.js
@@ -5,7 +5,15 @@ export const main = document.querySelector('#main');
 
 const login = new Login();
 const token = sessionStorage.getItem("x-token");
-if (token) login.autoLogin(token);
+if (token) 
+{
+    login.autoLogin(token)
+    .then(() => {})
+    .catch(err => 
+    {
+        main.appendChild(login.render())
+    });
+}
 else main.appendChild(login.render());
 
 //Tu leci główny kod Apki
